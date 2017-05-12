13c10d4f8788fbb928a67523802b64acf3514e72
negarIravani <negar.iravanii@gmail.com>
2017-05-12 20:07:41 +0430

    fast version

--- lsh.py
+++ lsh.py
@@ -1,14 +1,15 @@
-''' Locality Sensitive Hashing'''
-
 import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+import time
+from scipy.sparse import csr_matrix
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import csr_matrix
+
 from numpy import linalg as LA
 from copy import copy
 from  itertools import combinations
 import  sklearn.metrics.pairwise
-import time
+
 import json
 import os
 os.system('cls')
@@ -18,9 +19,6 @@ os.system('cls')
 wiki = pd.read_csv('people_wiki.csv')
 #insert a new column as an indicator
 wiki['id'] = np.array([i for i in range(wiki.shape[0])])
-wiki.info()
-
-
 # load the tf_idf file
 loader = np.load('people_wiki_tf_idf.npz')
 #### result is [shape, data, indices, indptr]
@@ -37,6 +35,19 @@ indptr = loader['indptr']
 # convert compress sparse raw(csr) to non compress
 tf_idf = csr_matrix( (data, indices, indptr), shape)
 
+
+#nearest neighbor modelwith Cosine distance as a distance measure,disrtance = 1 - cos(angle between)
+start1 = time.time()
+model_brute = NearestNeighbors(metric= 'cosine' , algorithm = 'brute')
+# use tf_idf as training set
+model_brute.fit(tf_idf)
+# 10 nearest neighbors to barack with all tfidf as a representation for text
+
+distances, indices = model_brute.kneighbors(tf_idf[35817], n_neighbors = 10)
+stop1 = time.time()
+time_brute = stop1 - start1
+print " it takes brute force search to find knn for one dtp", time_brute
+print " to find knn for all dpts it probably will take",time_brute*tf_idf.shape[0]
 
 def lsh_train( data, h = 16, seed = None):
     '''INPUT:
@@ -97,69 +108,14 @@ def lsh_train( data, h = 16, seed = None):
 
     return model
 
-# explore the results
+start2 = time.time()
 model = lsh_train( tf_idf,16, 143)
 hash_table = model['hash_table']
 data_bin_integer = model['data_bin_integer']
 data_bin_bit = model['data_bin_bit']
-
-print_result_1 = 0
-if print_result_1 == 1:
-    if   0 in hash_table and hash_table[0]   == [39583] and \
-       143 in hash_table and hash_table[143] == [19693, 28277, 29776, 30399]:
-        print 'Passed!'
-    else:
-        print ' failes : ('
-
-    barack =  wiki[wiki['name'] == 'Barack Obama']
-    print "barack obama id :%d" %(barack['id'])
-    print "bin(integer) that barack obama falls in :%d" %(data_bin_integer[35817])
-
-    biden =  wiki[wiki['name'] == 'Joe Biden']
-    print "joe biden id :%d" %(biden['id'])
-    print "bin of obama and biden : "
-    print data_bin_bit[35817]
-    print data_bin_bit[24478]
-
-    jones = wiki[wiki['name']=='Wynn Normington Hugh-Jones']
-    print "jones( a politician) id :%d" %(jones['id'])
-    print "bins of obama and jones :"
-    print data_bin_bit[35817] == data_bin_bit[22745]
-
-    print "articles that are in the same bin as barack obama : "
-    id_same_bin_as_barack = hash_table[50194]
-    print wiki.filter(items=id_same_bin_as_barack , axis =0)
-
-def cosine_distance(x,y):
-    ''' function to comput cosine distance
-    INPUT : two sparse matrixes (1 , D)
-    OUTPUT : cosine distance
-    '''
-    # change soarse matrixes to ordinary matrix
-    # this is neede for using norm function
-    n = x.todense()
-    m = y.todense()
-    # compute inner product
-    mn = m.dot(n.T)
-    # compute norm of two matrixes
-    n_norm = LA.norm(n)
-    m_norm = LA.norm(m)
-    #compute cosine between these two matrixes
-    similarity = mn[0,0] / (n_norm * m_norm)
-    # return 1 - cos(teta) as distance
-    return 1 - similarity
-
-print_result_2 = 0
-if print_result_2 == 1:
-    print "-"*30
-    print "cosine distance of obama from joe biden: % 15s:"\
-    %cosine_distance(tf_idf[35817],tf_idf[24478])
-
-    print "cosine distance of obama's page & another article in his bin :"
-    for id in id_same_bin_as_barack:
-        print cosine_distance(tf_idf[35817],tf_idf[id])
-
-
+stop2 = time.time()
+time_partitioning = stop2 - start2
+print " it took for partitioning ", time_partitioning
 
 def search_nearby_bins( q_bin_bits, hash_table, search_radius=2):
     ''' for a given query and a train_LSH, return all candidates\
@@ -194,13 +150,9 @@ def search_nearby_bins( q_bin_bits, hash_table, search_radius=2):
             candidates_list = candidates_list + hash_table[nearby_bin_integer]
     return candidates_list
 
-print_result_3 =0
-if print_result_3 == 1:
-    print "candidate list in 0 radius of query point "
-    print search_nearby_bins( data_bin_bit[35817], hash_table, search_radius=0)
-    print search_nearby_bins( data_bin_bit[35817], hash_table, search_radius=1)
 
-# main loop
+
+#main loop
 def knn_lsh( max_radius , q, k , model, data = tf_idf):
     '''
     for a given query and a maximun searching area , find knn
@@ -217,6 +169,7 @@ def knn_lsh( max_radius , q, k , model, data = tf_idf):
     '''
     # first Search nearby bins and collect candidates
     all_candidates = []
+    #start3 = time.time()
     for rad in range(max_radius+1):
         # for every acceptable radiunse, find candidate points
         candidates_rad = search_nearby_bins\
@@ -224,13 +177,14 @@ def knn_lsh( max_radius , q, k , model, data = tf_idf):
         # store all candidates'id in a list
         all_candidates = all_candidates + candidates_rad
 
+    #stop3 = time.time()
+    #print " takes to find all candidates", (stop3 - start3)
     # remove a query point from list of candidate
     all_candidates.remove(q)
     # pass through every candidate and calculate the distance between query and\
     # a given candidate
     # knn neighbors found so far, first element of tuple is distance, second:id
     # max number of nn is len(all_candidates)
-    start2=time.time()
     if k > len(all_candidates):
         k = len(all_candidates)
     # best_k = [(10,0)]*k
@@ -249,72 +203,69 @@ def knn_lsh( max_radius , q, k , model, data = tf_idf):
     # #simply calculate distance to all condidates and then sort them $ pick k
     # # use scikit distance
     # dis = sklearn.metrics.pairwise.cosine_distances(data[all_candidates], data[q])
-    b =[]
-    for can_id in all_candidates:
-         dis =cosine_distance(data[can_id], data[q])
-         b.append((dis,can_id))
-    b.sort(key=lambda x: x[0])
-    best_k = b[0:k]
-    stop2 = time.time()
-    time2= stop2 - start2
-    print " finding knn for all candidate takes", time2
-    return (best_k, len(all_candidates))
+    #start4 = time.time()
+    sim = sklearn.metrics.pairwise.cosine_similarity( data[q], data[all_candidates])
+    b = sim[0].argsort()[-k:][::-1]
+    best_k_sim = sim[0,b]
+    best_k_dis = 1 - best_k_sim
+    best_k_ind =  [all_candidates[j] for j in b]
 
-print_result_4 = 1
+    #stop4 = time.time()
+    #print " finding knn for all candidate takes", (stop4 - start4)
+    return (best_k_dis, best_k_ind, len(all_candidates))
+
+print_result_4 = 0
 if print_result_4 ==1:
-    (a,b) = knn_lsh( 4 , 35817, 10 , model, tf_idf)
-    print " number of candidates check within 3 bites : %d" %b
+    (a,b,c) = knn_lsh( 3 , 35817, 10 , model, tf_idf)
+    print " number of candidates check within 3 bites : %d" %c
 
     print " results of nearest neighbors, (distance, id)"
     for i in range(len((a))):
-        print "% 15f,% 10d, %20s" %(a[i][0],a[i][1], wiki.iloc[a[i][1]]['name'])
+        print "% 15f,% 10d, %20s" %(a[i],b[i], wiki.iloc[b[i]]['name'])
 
-#considering  different serach radiunse
-#initiate variables
+# effect of numeber of bins searching through
+
 time_history =[]
 num_candidates_history = []
 max_distance_to_query_history = []
 min_distance_to_query_history = []
 av_distance_to_query_history = []
 
-for max_search_radiunse in range(8):
+for max_search_radiunse in range(16):
     start = time.time()
     # find knn by searching through max_search_radiunse given
-    (dis_id, num_candidates) = knn_lsh\
+    (dis, id, num_candidates) = knn_lsh\
     ( max_search_radiunse, q = 35817, k =10, model = model, data =tf_idf)
     stop = time.time()
     # for 10 nn, print their distance to query, their name and id
     print " search_raduinse : ", max_search_radiunse
-    for i in range(len((dis_id))):
-        print "% 15f,% 10d, %20s" %(dis_id[i][0],dis_id[i][1], wiki.iloc[dis_id[i][1]]['name'])
+    for i in range(len((dis))):
+        print "% 15f,% 10d, %20s" %(dis[i],id[i], wiki.iloc[id[i]]['name'])
 
     # neasure time
     time_lentgh = stop - start
     # collect results for differenr search radiunse
     time_history.append(time_lentgh)
     num_candidates_history.append(num_candidates)
-    max_distance_to_query_history.append(max(dis_id,key=lambda item:item[0])[0])
-    min_distance_to_query_history.append(min([x[0] for x in dis_id]))
-    av_distance_to_query_history.append(sum([x[0] for x in dis_id])/len(dis_id))
-
-print max_distance_to_query_history
-print min_distance_to_query_history
-print av_distance_to_query_history
+    max_distance_to_query_history.append(max(dis))
+    min_distance_to_query_history.append(min(dis))
+    av_distance_to_query_history.append(sum(dis)/len(dis))
 
 
-plt.plot(num_candidates_history)
+
+plt.plot(num_candidates_history, linewidth = 4)
 plt.ylabel( 'number of candidates searched')
 plt.xlabel( 'search radiunse ')
 
 plt.figure()
-plt.plot(time_history)
+plt.plot(time_history, linewidth = 4)
 plt.ylabel( 'time')
 plt.xlabel( 'search radiunse ')
 
 plt.figure()
-plt.plot(max_distance_to_query_history, label = 'maximum distance to query point')
-plt.plot(min_distance_to_query_history, label = 'minimum distance to query point')
-plt.plot(av_distance_to_query_history, label = 'average distance to query point')
+plt.plot(max_distance_to_query_history, linewidth = 4, label = 'maximum distance to query point')
+plt.plot(min_distance_to_query_history, linewidth = 4, label = 'minimum distance to query point')
+plt.plot(av_distance_to_query_history, linewidth = 4, label = 'average distance to query point')
 plt.ylabel( 'cosine distance to query')
 plt.xlabel( 'search radiunse ')
 plt.legend(loc ='best')
